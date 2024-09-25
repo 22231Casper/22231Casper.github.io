d2e4898bb76997f7891bab0d815ae40d2c6cb567
22231 Casper <22231.northcote.school.nz>
2024-09-26 09:12:38 +1200

    push

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 alert("bbq burger")
 
 
-javascript:images = ['images/Freaky Casper.png', 'images/Group walk.png', 'images/Deep Fried Casper.png', 'images/casper.png', 'images/Casper on Mount Everest.jpg'];javascript:stuff = document.getElementsByTagName("img");for (var i = 0; i<stuff.length; i++) {stuff[i].src = ('https://22231casper.github.io' + images[i % 5]);}%20void%200 
+javascript:let images = ['images/Freaky Casper.png', 'images/Group walk.png', 'images/Deep Fried Casper.png', 'images/casper.png', 'images/Casper on Mount Everest.jpg'];let stuff = document.getElementsByTagName("img");for (var i = 0; i<stuff.length; i++) {alert('https://22231casper.github.io/' + images[math.floor(math.random()*images.length)]);} void 0
 
 
