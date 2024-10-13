158086f5be902f9afc19df741416997730c7e135
22231 Casper <22231.northcote.school.nz>
2024-10-14 12:40:09 +1300

    blombus

--- scripts/test.js
+++ scripts/test.js
@@ -2,3 +2,5 @@ alert("bbq burger")
 
 javascript: images = ['images/Freaky Casper.png', 'images/money dollar.gif', 'images/John Mugshot.jpg', 'images/John Mews.png', 'images/Group Photo.jpg', 'images/car-crash.gif', 'images/Group walk.png', 'images/Deep Fried Casper.png', 'images/casper.png', 'images/Casper on Mount Everest.jpg']; stuff = document.getElementsByTagName("img"); for (var i = 0; i<stuff.length; i++) {stuff[i].src = 'https://22231casper.github.io/' + images[Math.floor(Math.random()*images.length)];} void 0
 
+// pingo pong                                                                                                   t.
+let bingo = "bango jango blombus";
